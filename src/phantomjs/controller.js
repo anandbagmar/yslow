9b52df9006375778db67fbccf53671f41f87b87d
soulgalore <peter@soulgalore.com>
2014-03-17 10:12:02 +0100

    added the old HAR creation, to be able to release 2.6

--- src/phantomjs/controller.js
+++ src/phantomjs/controller.js
@@ -15,6 +15,107 @@
 
 // For using yslow in phantomjs, see instructions @ http://yslow.org/phantomjs/
 
+
+// FROM netsniff.js borrowed from PhantomJS
+// https://github.com/ariya/phantomjs/blob/master/examples/netsniff.js
+if (!Date.prototype.toISOString) {
+    Date.prototype.toISOString = function () {
+        function pad(n) { return n < 10 ? '0' + n : n; }
+        function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
+        return this.getFullYear() + '-' +
+            pad(this.getMonth() + 1) + '-' +
+            pad(this.getDate()) + 'T' +
+            pad(this.getHours()) + ':' +
+            pad(this.getMinutes()) + ':' +
+            pad(this.getSeconds()) + '.' +
+            ms(this.getMilliseconds()) + 'Z';
+    }
+}
+
+function createHAR(address, title, startTime, resources, loadTime)
+{
+    var entries = [];
+
+    resources.forEach(function (resource) {
+        var request = resource.request,
+            startReply = resource.startReply,
+            endReply = resource.endReply;
+
+        if (!request || !startReply || !endReply) {
+            return;
+        }
+
+        // Exclude Data URI from HAR file because
+        // they aren't included in specification
+        if (request.url.match(/(^data:image\/.*)/i)) {
+            return;
+    }
+
+        entries.push({
+            startedDateTime: request.time.toISOString(),
+            time: endReply.time - request.time,
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: endReply.status,
+                statusText: endReply.statusText,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: endReply.headers,
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: startReply.bodySize,
+                content: {
+                    size: startReply.bodySize,
+                    mimeType: endReply.contentType
+                }
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: startReply.time - request.time,
+                receive: endReply.time - startReply.time,
+                ssl: -1
+            },
+            pageref: address
+        });
+    });
+
+    return {
+        log: {
+            version: '1.2',
+            creator: {
+                name: "PhantomJS",
+                version: phantom.version.major + '.' + phantom.version.minor +
+                    '.' + phantom.version.patch
+            },
+            pages: [{
+                startedDateTime: startTime.toISOString(),
+                id: address,
+                title: title,
+                pageTimings: {
+                    onLoad: loadTime
+                }
+            }],
+            entries: entries
+        }
+    };
+}
+
+
+// END from netsniff.js
+
 // variables for loading Restful web application completely.
 var resourceWait = 1e3,
     maxRenderWait = 1e4,
@@ -39,7 +140,8 @@ var i, arg, page, urlCount, viewport,
         threshold: 80,
         cdns: '',
         basicauth: '',
-        cookie: undefined
+        cookie: undefined,
+        harfilename: ''
     },
     unaryArgs = {
         help: false,
@@ -62,7 +164,8 @@ var i, arg, page, urlCount, viewport,
         t: 'threshold',
         ch: 'headers',
         ba: 'basicauth',
-        C: 'cookie'
+        C: 'cookie',
+        n: 'harfilename'
     };
 
 // loop args
@@ -179,6 +282,20 @@ urls.forEach(function (url) {
 
 	    // page load time
 	    loadTime = new Date() - startTime;
+
+      // generate HAR
+
+      page.title = page.evaluate(function () {
+          return document.title;
+      });
+      har = createHAR(page.address, page.title, page.startTime, page.harresources, loadTime);
+
+      var fs = require('fs');
+      if (yslowArgs.harfilename.length>0) {
+          if (!fs.exists(yslowArgs.harfilename))
+              fs.write(yslowArgs.harfilename,JSON.stringify(har, undefined, 4),'w');
+      }
+      // end generate har
 
 		    // set resources response time
 		    for (url in resources) {
@@ -488,6 +605,7 @@ urls.forEach(function (url) {
     }
 
     page.resources = {};
+    page.harresources = [];
 
     // allow x-domain requests, used to retrieve components content
     page.settings.webSecurityEnabled = false;
@@ -507,8 +625,17 @@ urls.forEach(function (url) {
         page.resources[req.url] = {
             request: req
         };
+
+        // when getting the HAR
+        page.harresources[req.id] = {
+            request: req,
+            startReply: null,
+            endReply: null
+        };
+
         noOfResources += 1;
-        clearTimeout(renderTimeout)
+        clearTimeout(renderTimeout);
+
     };
 
     // response
@@ -516,8 +643,16 @@ urls.forEach(function (url) {
         var info,
             resp = page.resources[res.url].response;
 
+        // for HAR
+         if (res.stage === 'start') {
+           page.harresources[res.id].startReply = res;
+        }
+
         //  hack for taking care of redirects
          if (res.stage === 'end' ) {
+
+            page.harresources[res.id].endReply = res;
+
             if (res.status === 301 || res.status === 302) {
                 var locationValue;
                 for (var i = 0; i < res.headers.length; i++) {
